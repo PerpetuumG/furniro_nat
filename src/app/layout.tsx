447d891294742a74ae6e2ab1bbde7e066b583a20
PerpetuumG <perpetuumg@gmail.com>
2024-06-19 15:12:52 +0300

    create project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '700'] });
+
+export const metadata: Metadata = {
+  title: "Furniro Ecommerce",
+  description: "Get any furniture items on the fly",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
