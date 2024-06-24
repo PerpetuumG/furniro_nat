56b7a6ab2aeaebe307c329a43eec2af7b818d4b3
PerpetuumG <perpetuumg@gmail.com>
2024-06-24 12:56:12 +0300

    ShopPaginationSection

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,6 +1,7 @@
 import ShopHeroSection from '@/components/sections/shop/ShopHeroSection';
 import ShopFilterSection from '@/components/sections/shop/ShopFilterSection';
 import ShopProductSection from '@/components/sections/shop/ShopProductSection';
+import ShopPaginationSection from '@/components/sections/shop/ShopPaginationSection';
 import ShopBannerSection from '@/components/sections/shop/ShopBannerSection';
 
 const ShopPage = () => {
@@ -9,6 +10,9 @@ const ShopPage = () => {
       <ShopHeroSection />
       <ShopFilterSection />
       <ShopProductSection />
+      <div className={'my-[70px]'}>
+        <ShopPaginationSection />
+      </div>
       <ShopBannerSection />
     </div>
   );
--- src/components/sections/shop/ShopPaginationSection.tsx
+++ src/components/sections/shop/ShopPaginationSection.tsx
@@ -1,9 +1,34 @@
-const ShopPaginationSection = () => {
-    return (
-        <div>
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+} from '@/components/ui/pagination';
 
-        </div>
-    );
+const ShopPaginationSection = () => {
+  return (
+    <section>
+      <Pagination>
+        <PaginationContent className={'flex gap-[38px]'}>
+          <PaginationItem>
+            <PaginationLink href='/shop?page=1'>1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href='/shop?page=2' isActive>
+              2
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href='/shop?page=3'>3</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href='/shop?type=next' />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </section>
+  );
 };
 
 export default ShopPaginationSection;
