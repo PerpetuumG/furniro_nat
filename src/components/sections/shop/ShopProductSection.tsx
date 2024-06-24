23afa44060bed11885dfe3e56f621677474c7624
PerpetuumG <perpetuumg@gmail.com>
2024-06-24 12:30:33 +0300

    ShopProductSection

--- src/components/sections/shop/ShopProductSection.tsx
+++ src/components/sections/shop/ShopProductSection.tsx
@@ -1,9 +1,16 @@
-const ShopProductSection = () => {
-    return (
-        <div>
+import { PRODUCTS } from '@/lib/constants';
+import ProductCard from '@/components/cards/ProductCard';
 
-        </div>
-    );
+const ShopProductSection = () => {
+  return (
+    <section className={''}>
+      <div className={'grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 mt-[46px] px-16'}>
+        {PRODUCTS.map((item, index) => (
+          <ProductCard {...item} key={index} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default ShopProductSection;
