6c89a87fb7084cae234103db09fb947ecf9d8b58
PerpetuumG <perpetuumg@gmail.com>
2024-06-27 16:05:19 +0300

    CartSection

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -5,8 +5,11 @@ import { MenuIcon, X } from 'lucide-react';
 import Link from 'next/link';
 import Logo from '../../../public/images/logo.png';
 import Image from 'next/image';
+import CartSection from '@/components/sections/shop/CartSection';
+import { RemoveScroll } from 'react-remove-scroll';
 
 function NavBar() {
+  const [showCart, setShowCart] = useState(false);
   const [menu, setMenu] = useState(false);
   const links = [
     { title: 'Home', link: '/' },
@@ -34,7 +37,7 @@ function NavBar() {
     {
       iconUrl: '/images/cart_icon.png',
       alt: 'cart icon',
-      action: () => console.log('You just clicked on the cart icon'),
+      action: () => setShowCart(prevState => !prevState),
     },
   ];
 
@@ -43,72 +46,73 @@ function NavBar() {
   };
 
   return (
-    <div className='md:sticky md:top-0   md:shadow-none z-20 '>
-      {/* DESKTOP */}
-      <div className=' hidden lg:block animate-in fade-in zoom-in bg-white p-4'>
-        <div className='flex justify-between mx-[41px] items-center'>
-          <div>
-            <Image src={Logo} alt='logo' />
-          </div>
-          <div className='flex gap-[20px] xl:gap-[50px] text-[16px] items-center select-none'>
-            {links.map((link, index) => (
-              <Link
-                key={index}
-                href={link.link}
-                className={`hover:text-primary cursor-pointer flex items-center gap-2  font-[500] text-gray`}
-              >
-                <p>{link.title}</p>
-              </Link>
-            ))}
-          </div>
-          <div className='flex items-center gap-[40px] select-none'>
-            {icons.map((icon, index) => (
-              <img
-                src={icon.iconUrl}
-                alt={icon.alt}
-                onClick={icon.action}
-                key={index}
-                className={'cursor-pointer'}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-
-      {/* MOBILE */}
-      <div
-        className={` block lg:hidden shadow-sm  fixed top-0 w-full z-[999] bg-white py-4 animate-in fade-in zoom-in  ${
-          menu ? ' !bg-primary-light py-2' : ''
-        } `}
-      >
-        <div className='flex justify-between mx-[10px]'>
-          <div className='flex gap-[50px] text-[16px] items-center select-none'>
-            <Image src={Logo} alt='logo' className='w-[7rem]' />
-          </div>
-          <div className='flex items-center gap-[40px]'>
-            {menu ? (
-              <X
-                className='cursor-pointer animate-in fade-in zoom-in text-black'
-                onClick={toggleMenu}
-              />
-            ) : (
-              <MenuIcon
-                className='cursor-pointer animate-in fade-in zoom-in'
-                onClick={toggleMenu}
-              />
-            )}
-          </div>
-        </div>
-        {menu ? (
-          <div className='my-8 select-none animate-in slide-in-from-right'>
-            <div className='flex flex-col gap-8 mt-8 mx-4'>
+    <div className={'relative'}>
+      <div className='md:sticky md:top-0 md:shadow-none z-20 relative'>
+        {/* DESKTOP */}
+        <div className=' hidden lg:block animate-in fade-in zoom-in bg-white p-4'>
+          <div className='flex justify-between mx-[41px] items-center'>
+            <div>
+              <Image src={Logo} alt='logo' />
+            </div>
+            <div className='flex gap-[20px] xl:gap-[50px] text-[16px] items-center select-none'>
               {links.map((link, index) => (
-                <Link key={index} href={link.link} className={`text-black cursor-pointer`}>
+                <Link
+                  key={index}
+                  href={link.link}
+                  className={`hover:text-primary cursor-pointer flex items-center gap-2  font-[500] text-gray`}
+                >
                   <p>{link.title}</p>
                 </Link>
               ))}
+            </div>
+            <div className='flex items-center gap-[40px] select-none'>
+              {icons.map((icon, index) => (
+                <img
+                  src={icon.iconUrl}
+                  alt={icon.alt}
+                  onClick={icon.action}
+                  key={index}
+                  className={'cursor-pointer'}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
 
-              <div className='flex flex-col gap-[40px] select-none'>
+        {/* MOBILE */}
+        <div
+          className={` block lg:hidden shadow-sm  fixed top-0 w-full z-[999] bg-white py-4 animate-in fade-in zoom-in  ${
+            menu ? ' !bg-primary-light py-2' : ''
+          } `}
+        >
+          <div className='flex justify-between mx-[10px]'>
+            <div className='flex gap-[50px] text-[16px] items-center select-none'>
+              <Image src={Logo} alt='logo' className='w-[7rem]' />
+            </div>
+            <div className='flex items-center gap-[40px]'>
+              {menu ? (
+                <X
+                  className='cursor-pointer animate-in fade-in zoom-in text-black'
+                  onClick={toggleMenu}
+                />
+              ) : (
+                <MenuIcon
+                  className='cursor-pointer animate-in fade-in zoom-in'
+                  onClick={toggleMenu}
+                />
+              )}
+            </div>
+          </div>
+          {menu ? (
+            <div className='my-8 select-none animate-in slide-in-from-right'>
+              <div className='flex flex-col gap-8 mt-8 mx-4'>
+                {links.map((link, index) => (
+                  <Link key={index} href={link.link} className={`text-black cursor-pointer`}>
+                    <p>{link.title}</p>
+                  </Link>
+                ))}
+
+                <div className='flex flex-col gap-[40px] select-none'>
                   {icons.map((icon, index) => (
                     <img
                       src={icon.iconUrl}
@@ -118,11 +122,30 @@ function NavBar() {
                       className={'cursor-pointer w-[28px] h-[28px] object-contain'}
                     />
                   ))}
+                </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <div></div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+
+      {/*  */}
+      {showCart && (
+        <div
+          className={
+            'hidden md:block absolute animate-out left-0 right-0 top-0 h-screen bg-black/20 z-[99]'
+          }
+          onClick={() => setShowCart(prevState => !prevState)}
+        ></div>
+      )}
+
+      <div className={'hidden md:block md:absolute top-0 right-0 z-[100]'}>
+        {showCart && (
+          <RemoveScroll>
+            <CartSection toggleShowCart={() => setShowCart(prevState => !prevState)} />
+          </RemoveScroll>
         )}
       </div>
     </div>
---
+++ src/components/sections/shop/CartSection.tsx
@@ -0,0 +1,118 @@
+import { Separator } from '@/components/ui/separator';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import MainButton from '@/components/common/MainButton';
+
+const CartSection = ({ toggleShowCart }: { toggleShowCart: () => void }) => {
+  const data = [
+    {
+      id: 1,
+      productImageUrl: '/images/sofa.png',
+      productName: 'Asgaard sofa',
+      quantity: 1,
+      unitPrice: 25000000,
+    },
+    {
+      id: 2,
+      productImageUrl: '/images/sofa.png',
+      productName: 'Casaiving Wood',
+      quantity: 1,
+      unitPrice: 27000000,
+    },
+  ];
+
+  const [products, setProducts] = useState(data);
+  const [subTotal, setSubTotal] = useState(0);
+  const router = useRouter();
+
+  const removeProductFromCart = (productId: number | string) => {
+    const filterProducts = products.filter(product => product.id !== productId);
+    setProducts(filterProducts);
+  };
+
+  const computeSubtitle = () => {
+    let total = 0;
+    for (const product of products) {
+      total += product.quantity * product.unitPrice;
+    }
+    setSubTotal(total);
+  };
+
+  useEffect(() => {
+    computeSubtitle();
+  }, [products]);
+
+  return (
+    <div className={'w-[417px] h-[746px] bg-white p-[30px] flex justify-between flex-col'}>
+      <div>
+        <div className={'flex justify-between items-center mb-9'}>
+          <p className={'font-semibold text-[24px]'}>Shopping Cart</p>
+          <div className={'hover:cursor-pointer'} onClick={toggleShowCart}>
+            <img src={'/images/cart_alt_icon.png'} alt='cart_icon' />
+          </div>
+        </div>
+
+        <Separator />
+
+        <div className={'mt-6 flex flex-col gap-5'}>
+          {products.map(product => (
+            <div key={product.id} className={'flex items-center gap-[32px] w-full justify-between'}>
+              <div>
+                <img
+                  src={product.productImageUrl}
+                  alt={product.productName}
+                  className={'w-[108px] h-[105px] rounded-[1rem] object-cover'}
+                />
+              </div>
+
+              <div>
+                <p className={'text-normal'}>{product.productName}</p>
+
+                <p>
+                  {product.quantity} X{' '}
+                  <span className={'text-primary font-medium text-sm'}>
+                    {' '}
+                    Rs. {product.unitPrice}
+                  </span>
+                </p>
+              </div>
+
+              <div className={'cursor-pointer'} onClick={() => removeProductFromCart(product.id)}>
+                <img src={'/images/delete_icon.png'} alt='delete' />
+              </div>
+            </div>
+          ))}
+
+          {products.length === 0 && (
+            <div className={'flex justify-center mt-8 text-gray-400'}>No product is cart</div>
+          )}
+        </div>
+      </div>
+
+      <div>
+        <div className={'flex justify-between mb-[23px]'}>
+          <p>Subtotal: </p>
+          <p className={'text-primary text-normal font-semibold'}>Rs. {subTotal}</p>
+        </div>
+
+        {subTotal ? (
+          <div>
+            <Separator />
+            <div className={'mt-8 flex justify-center'}>
+              <MainButton
+                text={'Checkout'}
+                classes={
+                  'bg-white hover:bg-white text-black  border border-black rounded-[50px] h-[40px] w-[150px]'
+                }
+              />
+            </div>
+          </div>
+        ) : (
+            <div></div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CartSection;
